3735db3235035c4eb8382b9f7d2b66384b35f653
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2022-12-16 18:48:10 -0300

    Primeiro Commit

---
+++ AppNumerosPrimos/AppNumerosPrimos/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Globalization;
+
+namespace AppNumerosPrimos
+{
+    public partial class MainPage : ContentPage
+    {
+        string NumerosPrimos = "";
+        int n1 = 1;
+        int n2 = 10;
+
+        for (int numeroVez = n1; numeroVez< = n2; numeroVez++)
+            {
+              int dividiu = 0;
+        for (int divisor= 1; divisor <= numeroVez; divisor++)
+            {
+              if (numeroVez % divisor == 0)
+               { 
+                 dividiu++;
+               }
+            }
+       if (dividiu  == 2)
+       {
+          numerosPrimos += (numerosPrimos == "" ?  )
+        }
